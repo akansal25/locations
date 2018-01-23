18355e89d3ad148465e458ffa517da9bf0175b8f
Abhinav Kansal <abhinavkansal@gmail.com>
2018-01-23 05:46:18 +0530

    Add new Pojos

---
+++ src/main/java/entity/CutOverHours.java
@@ -0,0 +1,15 @@
+package entity;
+
+
+public class CutOverHours {
+    private String dayoftheweek;
+
+    public String getDayoftheweek() {
+        return dayoftheweek;
+    }
+
+    public void setDayoftheweek(String dayoftheweek) {
+        this.dayoftheweek = dayoftheweek;
+    }
+
+}
---
+++ src/main/java/entity/DriveUpHours.java
@@ -0,0 +1,14 @@
+package entity;
+
+public class DriveUpHours {
+
+    private String dayoftheweek;
+
+    public String getDayoftheweek() {
+        return dayoftheweek;
+    }
+
+    public void setDayoftheweek(String dayoftheweek) {
+        this.dayoftheweek = dayoftheweek;
+    }
+}
